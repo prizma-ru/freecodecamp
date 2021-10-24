32baf3c7f1f47155cc39fa08dfa1801d531f44ad
prizma-ru <dmitry.glotov@gmail.com>
2021-10-24 21:50:16 +0300

    feat: add task Roman Numeral ConverterPassed

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,19 +1,65 @@
 function convertToRoman(num) {
-    const romanN = {
+    const unitsRoman = {
         1: "I",
+        2: "II",
+        3: "III",
+        4: "IV",
         5: "V",
-        10: "X",
-        50: "L",
-        100: "C",
-        500: "D",
-        1000: "M",
-
+        6: "VI",
+        7: "VII",
+        8: "VIII",
+        9: "IX",
+        0: ""
     }
+    const dozensRoman = {
+        1: "X",
+        2: "XX",
+        3: "XX",
+        4: "XL",
+        5: "L",
+        6: "LX",
+        7: "LXX",
+        8: "LXXX",
+        9: "XC",
+        0: "",
+    }
+    const hundredsRoman = {
+        1: "C",
+        2: "CС",
+        3: "CС",
+        4: "CD",
+        5: "D",
+        6: "DC",
+        7: "DCC",
+        8: "DCCC",
+        9: "CM",
+        0: "",
+    }
+    const thousand = (str) => {
+        let strRoman = '';
+        for (let index = 0; index < Number(str); index++) {
+            strRoman += 'M';
 
-    return romanN[num];
+        }
+        return strRoman;
+    }
+    const arrRoman = (num) => (`${num}`).split('');
+    const masRoman = [...arrRoman(num)];
+    if (num < 10) {
+        return unitsRoman[num];
+    }
+    if (num < 100) {
+        return dozensRoman[masRoman[0]] + unitsRoman[masRoman[1]];
+    }
+    if (num < 1000) {
+        return hundredsRoman[masRoman[0]] + dozensRoman[masRoman[1]] + unitsRoman[masRoman[2]];
+    }
+    if (num < 10000) {
+        return thousand(masRoman[0]) + hundredsRoman[masRoman[1]] + dozensRoman[masRoman[2]] + unitsRoman[masRoman[3]];
+    }
 }
 
 
-console.log(convertToRoman(10));
+console.log(convertToRoman(400) === 'CD');
 
-console.log(3900%1000);
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,34 +1,64 @@
-function caesarsCipher(str) {
-    let ROT13 = {
-        N: 'A',
-        O: 'B',
-        P: 'C',
-        Q: 'D',
-        R: 'E',
-        S: 'F',
-        T: 'G',
-        U: 'H',
-        V: 'I',
-        W: 'J',
-        X: 'K',
-        Y: 'L',
-        Z: 'M',
-        A: 'N',
-        B: 'O',
-        C: 'P',
-        D: 'Q',
-        E: 'R',
-        F: 'S',
-        G: 'T',
-        H: 'U',
-        I: 'V',
-        J: 'W',
-        K: 'X',
-        L: 'Y',
-        M: 'Z',
+function convertToRoman(num) {
+    const unitsRoman = {
+        1: "I",
+        2: "II",
+        3: "III",
+        4: "IV",
+        5: "V",
+        6: "VI",
+        7: "VII",
+        8: "VIII",
+        9: "IX",
+        0: ""
     }
-    return str.split("").map(a => ROT13[a] !== undefined ? ROT13[a] : a).join("");
+    const dozensRoman = {
+        1: "X",
+        2: "XX",
+        3: "XX",
+        4: "XL",
+        5: "L",
+        6: "LX",
+        7: "LXX",
+        8: "LXXX",
+        9: "XC",
+        0: "",
+    }
+    const hundredsRoman = {
+        1: "C",
+        2: "CС",
+        3: "CС",
+        4: "CD",
+        5: "D",
+        6: "DC",
+        7: "DCC",
+        8: "DCCC",
+        9: "CM",
+        0: "",
+    }
+    const thousand = (str) => {
+        let strRoman = '';
+        for (let index = 0; index < Number(str); index++) {
+            strRoman += 'M';
+
+        }
+        return strRoman;
+    }
+    const arrRoman = (num) => (`${num}`).split('');
+    const masRoman = [...arrRoman(num)];
+    if (num < 10) {
+        return unitsRoman[num];
+    }
+    if (num < 100) {
+        return dozensRoman[masRoman[0]] + unitsRoman[masRoman[1]];
+    }
+    if (num < 1000) {
+        return hundredsRoman[masRoman[0]] + dozensRoman[masRoman[1]] + unitsRoman[masRoman[2]];
+    }
+    if (num < 10000) {
+        return thousand(masRoman[0]) + hundredsRoman[masRoman[1]] + dozensRoman[masRoman[2]] + unitsRoman[masRoman[3]];
+    }
 }
 
-console.table(caesarsCipher("SERR PBQR PNZC"));
+
+console.log(convertToRoman(400) === 'CD');
 
